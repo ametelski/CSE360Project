f4b172349006e7cba5ce1f61b1e491f32428f3e4
Adam Metelski <asmetelski@gmail.com>
2016-03-31 15:32:04 -0700

    commit from Adam Metelski.

--- DiceProject/src/cse360project/Dice.java
+++ DiceProject/src/cse360project/Dice.java
@@ -2,7 +2,7 @@ package cse360project;
 /**
  * Liar's Dice Game
  * 
- * @author Nicole Gettelman
+ * @author Nicole Gettelman, Adam Metelski
  * @version March 31, 2016
  *
  */
@@ -10,5 +10,7 @@ package cse360project;
 
 public class Dice 
 {
-
+	public static void main(String []args){
+		System.out.println("Adam Metelski was here!");
+	}
 }
