6e8db2e2e0bf93bcf11bd39188a63aaf0804fe9c
Miguel Deniz Lopez <miguel391@yahoo.com>
2016-03-31 19:13:43 -0700

    Miguel Deniz - 1st Commit

--- DiceProject/src/cse360project/Dice.java
+++ DiceProject/src/cse360project/Dice.java
@@ -5,6 +5,7 @@ package cse360project;
  * @author Nicole Gettelman
  * @author Adam Metelski
  * @author Zackary Crosley
+ * @author Miguel Lopez
  * @version March 31, 2016
  *
  */
@@ -15,5 +16,6 @@ public class Dice
 	public static void main(String []args){
 		System.out.println("Adam Metelski was here!");
 		System.out.println("Zackary Crosley standing by.");
+		System.out.printf("Miguel Deniz\n");
 	}
 }
